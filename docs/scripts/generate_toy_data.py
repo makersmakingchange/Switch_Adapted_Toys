7100e529a09e4796d4508c6baec9e5d4631ba842
tjfentie <tylerfentie@gmail.com>
2026-07-14 17:05:03 -0600

    Update generate_toy_data.py

--- docs/scripts/generate_toy_data.py
+++ docs/scripts/generate_toy_data.py
@@ -125,7 +125,7 @@ def main():
                 shutil.copyfile(thumbnail, IMAGES_OUT_DIR / image_filename)
                 image_path = f"images/toys/{image_filename}"
             else:
-                image_path = "images/toys/placeholder.jpg"
+                image_path = "images/placeholder.jpg"
                 print(f"NOTE: no thumbnail found for '{name}' - using placeholder.")
 
             toys.append(
